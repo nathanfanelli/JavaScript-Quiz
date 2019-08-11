f37245cb2143d01585f2aec1455fa3ea6a6d1649
nathanfanelli <48001451+nathanfanelli@users.noreply.github.com>
2019-08-11 12:28:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,114 @@
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+var myQuestions = [
+	{
+		question: "What is the city Batman protects?",
+		answers: {
+			A: 'Chicago',
+			B: 'Gotham',
+			C: 'New York'
+		},
+		correctAnswer: 'B'
+	},
+	{
+		question: "What is Batman's true identity?",
+		answers: {
+			A: 'Matches Malone',
+			B: 'Christian Bale',
+			C: 'Bruce Wayne'
+		},
+		correctAnswer: 'C'
+    },
+    {
+		question: "Does Batman have a sidekick?",
+		answers: {
+			A: 'Of course! Robin!',
+			B: 'Batman doesn\'t need a sidekick.',
+			C: 'Who the hell is Batman?'
+		},
+		correctAnswer: 'A'
+    }
+];
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+        // we'll need a place to store the output and the answer choices
+        var output = [];
+        var answers;
+    
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+            
+            // first reset the list of answers
+            answers = [];
+    
+            // for each available answer to this question...
+            for(letter in questions[i].answers){
+    
+                // ...add an html radio button
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    + '</label>'
+                );
+            }
+    
+            // add this question and its answers to the output
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+    
+        // finally combine our output list into one string of html and put it on the page
+        quizContainer.innerHTML = output.join('');
+    }
+
+	function showResults(questions, quizContainer, resultsContainer){
+	
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        
+        // keep track of user's answers
+        var userAnswer = '';
+        var numCorrect = 0;
+        
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+    
+            // find selected answer
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+            
+            // if answer is correct
+            if(userAnswer===questions[i].correctAnswer){
+                // add to the number of correct answers
+                numCorrect++;
+                
+                // color the answers green
+                answerContainers[i].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else{
+                // color the answers red
+                answerContainers[i].style.color = 'red';
+            }
+        }
+    
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+
+	// show the questions
+	showQuestions(questions, quizContainer);
+
+	// when user clicks submit, show results
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
+}
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
